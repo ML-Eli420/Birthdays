121d526576cd36862838f02ce193d7120349ab3b
ML-Eli420 <elijahmilne686@gmail.com>
2023-02-07 15:37:52 +1000

    More Secure Password System

---
+++ app.js
@@ -0,0 +1,22 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    fetch("password.txt")
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.text();
+})
+.then(text => {
+  if (password === text) {
+    localStorage.setItem("authenticated", true);
+    window.location.href = "LoggedIn.html";
+  } else {
+    alert("Incorrect username or password");
+  }
+})
+.catch(error => console.error(error));
+  });
